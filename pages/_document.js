8c29533079ceb65134d2b91271ee860b76e632d9
Victor Fomenko <victorfomenko@me.com>
2017-09-20 22:41:24 +0300

    refact: add material ui

---
+++ pages/_document.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getContext from '../styles/getContext';
+
+//components
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+class MyDocument extends Document {
+    render() {
+      return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <meta name="viewport" content={
+              'user-scalable=0, initial-scale=1, ' +
+              'minimum-scale=1, width=device-width, height=device-height'
+            } />
+          <link rel="shortcut icon" href="/static/favicon.ico?v=3" />
+          <title>{'Crypto calculator'}</title>
+          <meta name="author" content="Victor Fomenko"/>
+          <meta name="keywords" content="Крипто калькулятор"/>
+          <meta name="description" content="Крипто калькулятор"/>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        </Head>
+        <body>
+          <Header/>
+          <Main />
+          <Footer/>
+          <NextScript />
+        </body>
+      </html>
+      )
+  }
+}
+
+MyDocument.getInitialProps = ctx => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. page.getInitialProps
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the server with error:
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. page.getInitialProps
+  // 3. page.render
+
+  // Get the context to collected side effects.
+  const context = getContext();
+  const page = ctx.renderPage(Component => props => (
+    <JssProvider registry={context.sheetsRegistry} jss={context.jss}>
+      <Component {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    stylesContext: context,
+    styles: (
+      <style
+        id="jss-server-side"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: context.sheetsRegistry.toString() }}
+      />
+    ),
+  };
+};
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,39 @@
-export default () => (
-  <div>Welcome to cryptocalc!</div>
-)
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from 'material-ui/Dialog';
+import Typography from 'material-ui/Typography';
+import withStyles from 'material-ui/styles/withStyles';
+import withRoot from '../components/WithRoot';
+
+const styles = {
+  root: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+class Index extends Component {
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <Typography type="display1" gutterBottom>
+          Crypto calculator
+        </Typography>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(Index));
