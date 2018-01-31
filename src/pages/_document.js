199e0cf3e13e1ed965126b90acd3a12fbe43c058
Victor Fomenko <victorfomenko@me.com>
2018-01-31 10:06:42 +0300

    add yandex metrika

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,6 +19,18 @@ class MyDocument extends Document {
           <title>Crypto calculator</title>
           <meta name="author" content="Victor Fomenko" />
           <link rel="stylesheet" href="/static/css/global.css" />
+          {/* Yandex.Metrika counter */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter47486365 = new Ya.Metrika2({ id:47486365, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/tag.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks2");`,
+            }}
+          />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<div><img src="https://mc.yandex.ru/watch/47486365" style="position:absolute; left:-9999px;" alt="" /></div>`,
+            }}
+          />
+          {/* Yandex.Metrika counter */}
         </Head>
         <body>
           <Main />
